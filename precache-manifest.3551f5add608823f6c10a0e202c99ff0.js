2fadaf4009686cf075e1b7e647b087c3758fe409
sean-code <john.nganga@student.moringaschool.com>
2023-02-18 19:46:04 +0300

    Updates

---
+++ precache-manifest.3551f5add608823f6c10a0e202c99ff0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1129c64015c4fe7999c",
+    "url": "/scholathon/static/css/main.5453a681.chunk.css"
+  },
+  {
+    "revision": "c1129c64015c4fe7999c",
+    "url": "/scholathon/static/js/main.c1129c64.chunk.js"
+  },
+  {
+    "revision": "a6313b5815c74bb4bb63",
+    "url": "/scholathon/static/js/1.a6313b58.chunk.js"
+  },
+  {
+    "revision": "cef0dffd389a17a05af3",
+    "url": "/scholathon/static/js/runtime~main.cef0dffd.js"
+  },
+  {
+    "revision": "bc9829fdb3ce66b9ac941261c2a4588c",
+    "url": "/scholathon/index.html"
+  }
+];
