e00b874001d6ec8badd2edbda7ab7291f69a52d1
sean-code <john.nganga@student.moringaschool.com>
2023-02-19 15:36:07 +0300

    Add footer section

--- src/components/setup/FooterPart.js
+++ src/components/setup/FooterPart.js
@@ -8,7 +8,7 @@ function Footer() {
           <div className="footer p-3 mt-4 text-center text-black" style={{color:'black', backgroundColor:'#fff', boxShadow:'0px 2px 10px', padding:'20px'}}>
             Developed With 🧡 By:
             <span className="text-warning font-weight-normal">
-              {' '} <a href='https://github.com/sean-code'>John Nganga </a>
+              {' '} <a href='https://github.com/sean-code' target='_blank' style={{color:'orange', textDecoration:'none'}}>John Nganga </a>
             </span> Using  React JS, Redux Js, OMDB
           </div>
         </div>
